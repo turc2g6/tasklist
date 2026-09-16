73614edbd939de67a0c9e57ccb91399bad60c857
turc2g6 <gigmber@gmail.com>
2026-09-16 14:37:23 +0200

    added saving to file

--- tasklist.py
+++ tasklist.py
@@ -1,5 +1,16 @@
-tasks = []
+import json
+from datetime import datetime
 
+
+def load_file():
+    with open("tasklist.json",'r',encoding='utf-8')as file:
+        tasklist=json.load(file)
+        
+        return tasklist
+    
+def save_file(tasklist):
+    with open("tasklist.json",'w',encoding='utf-8')as file:
+      json.dump(tasklist,file,indent=4,ensure_ascii=False)
 
 def show_actions():
     print("You are in main menu, choose your action :")
@@ -11,6 +22,7 @@ def show_actions():
 
 
 def show_tasks():
+    tasks=load_file()
     if not tasks:
         print("you don't have any tasks yet ")
         return
@@ -18,31 +30,70 @@ def show_tasks():
         print("task list:")
         counter = 1
         for task in tasks:
-            print(f"{counter}. {task}")
+            title=task['title']
+            description=task["description"]
+            if not description:
+                description="no description"
+            dead_line=task["dead_line"]
+            if dead_line=="None":
+                dead_line='no deadline'
+            created_at=task["created_at"]
+            is_done=task["is_done"]
+            if is_done:
+                is_done="✅"
+            else:
+                is_done="❌"
+            print(f"{counter}. {is_done} {title} \n   description: {description}\n   deadline: {dead_line}\n   created at: {created_at}")
             counter += 1
 
 
 def add_task():
+    tasks=load_file()
     title = input("enter the task name :")
-    tasks.append(title)
+    description=input('enter the task description (optional):')
+    while True:
+        dead_line=input('enter the task deadline in format YYYY-MM-DD (optional):')
+        if dead_line=="":
+            dead_line=None
+            break
+        try:
+            dead_line=datetime.strptime(dead_line,'%Y-%m-%d').date()
+            break
+        except ValueError:
+            print("wrong date format,must be YYYY-MM-DD ")
+    tasks.append({
+        'title':title,
+        'description':description,
+        'dead_line':str(dead_line),
+        'is_done':False,
+        'created_at':str(datetime.now().date())
+    })
+    save_file(tasks)
     print(f'task "{title}" was successfully added to the task list')
 
 
 def deleted_task():
+    tasks=load_file()
     show_tasks()
     number = int(input("enter the number of the task you want to delete:"))
+    if number<=0 or number>len(tasks):
+        print("no task with this number")
+        return
     tasks.pop(number - 1)
+    save_file(tasks)
     print("task deleted ")
 
 
-def check_task():
+def mark_task():
+    tasks=load_file()
     show_tasks()
     number = int(input("enter the number of the task you want to mark: "))
-    if "✅" in tasks[number - 1]:
-        tasks[number - 1] = tasks[number - 1][:-1]
-    else:
-        tasks[number - 1] = tasks[number - 1] + "✅"
-
+    if number<=0 or number>len(tasks):
+        print("no task with this number")
+        return
+    tasks[number - 1]["is_done"]= not tasks[number - 1]["is_done"]
+    save_file(tasks)
+    print("task marked")
 
 while True:
     show_actions()
@@ -57,4 +108,4 @@ while True:
     elif command == 3:
         deleted_task()
     elif command == 4:
-        check_task()
+        mark_task()
